8713a704645aa470d5cc650843bd255abb90756b
Israel Sanchez <israel.pro1994@gmail.com>
2019-07-26 02:13:15 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+ puts sum = num1 + num2
+ sum
 end
 
 def subtraction(num1, num2)
-
+  puts differance = num1 - num2
+differance
 end
 
 def division(num1, num2)
-
+  puts divide = num1 / num2
+divide
 end
 
 def multiplication(num1, num2)
-
+  puts average = num1 * num2
+average
 end
 
 def modulo(num1, num2)
-
+  puts differance = num1 % num2
+  differance
 end
 
 def square_root(num)
-
+  puts Math.sqrt(num)
+Math.sqrt(num)
 end
